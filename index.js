5afbb1e42fbe1fec8ce1ddf08bdb4ef9114e79a5
sparanzza <aurelio@sparanzza.com>
2019-07-08 15:24:36 +0200

    working with transformations

--- index.js
+++ index.js
@@ -1,62 +1,119 @@
 var canvas = document.getElementById("renderCanvas"); // Get the canvas element 
 var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
+
+/*********************************Start World Axes********************/
+var showAxis = function (size) {
+    var makeTextPlane = function (text, color, size) {
+        var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+        dynamicTexture.hasAlpha = true;
+        dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+        var plane = new BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+        plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+        plane.material.backFaceCulling = false;
+        plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+        plane.material.diffuseTexture = dynamicTexture;
+        return plane;
+    };
+
+    var axisX = BABYLON.Mesh.CreateLines("axisX", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+        new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+    ], scene);
+    axisX.color = new BABYLON.Color3(1, 0, 0);
+    var xChar = makeTextPlane("X", "red", size / 10);
+    xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+    var axisY = BABYLON.Mesh.CreateLines("axisY", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+    ], scene);
+    axisY.color = new BABYLON.Color3(0, 1, 0);
+    var yChar = makeTextPlane("Y", "green", size / 10);
+    yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+    var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
+        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+    ], scene);
+    axisZ.color = new BABYLON.Color3(0, 0, 1);
+    var zChar = makeTextPlane("Z", "blue", size / 10);
+    zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+};
+/***************************End World Axes***************************/
+
+/*******************************Local Axes****************************/
+function localAxes(size) {
+    var pilot_local_axisX = BABYLON.Mesh.CreateLines("pilot_local_axisX", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+        new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+    ], scene);
+    pilot_local_axisX.color = new BABYLON.Color3(1, 0, 0);
+
+    pilot_local_axisY = BABYLON.Mesh.CreateLines("pilot_local_axisY", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+        new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+    ], scene);
+    pilot_local_axisY.color = new BABYLON.Color3(0, 1, 0);
+
+    var pilot_local_axisZ = BABYLON.Mesh.CreateLines("pilot_local_axisZ", [
+        new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
+        new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+    ], scene);
+    pilot_local_axisZ.color = new BABYLON.Color3(0, 0, 1);
+
+    var local_origin = BABYLON.MeshBuilder.CreateBox("local_origin", { size: 1 }, scene);
+    local_origin.isVisible = false;
+
+    pilot_local_axisX.parent = local_origin;
+    pilot_local_axisY.parent = local_origin;
+    pilot_local_axisZ.parent = local_origin;
+
+    return local_origin;
+
+}
+/*******************************End Local Axes****************************/
+  
 /******* Add the create scene function ******/
 var createScene = function () {
 
             // Create the scene space
             var scene = new BABYLON.Scene(engine);
+            scene.clearColor = new BABYLON.Color3(.3, .3, .32);
+
             // Add a camera to the scene and attach it to the canvas
-            var camera = new BABYLON.ArcRotateCamera("Camera", BABYLON.Tools.ToRadians(0), BABYLON.Tools.ToRadians(0), 25, BABYLON.Vector3.Zero(), scene);
+            var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI, Math.PI / 8, 150, BABYLON.Vector3.Zero(), scene);
+            camera.setPosition(new BABYLON.Vector3(10, 3, -10));
             camera.attachControl(canvas, true);
-            // Add lights to the scene
-            var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(2, 2, 0), scene);
-            var light2 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 2, -2), scene);
+
             // Add and manipulate meshes in the scene
-            var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:3}, scene);
-            sphere.position.y = 1.5;
+            // var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1, width: 0.75, depth: 0.25}, scene);
+            var box;
+            var pitch, yaw, roll;
+            var numberOfBox = 20;
+            for (let index = 0; index < numberOfBox; index++) {
+                box = BABYLON.MeshBuilder.CreateBox("box", {height: 1, width: 0.75, depth: 0.25}, scene);
+                box.position.z = index;
+                box.rotation.z = Math.PI / index;
+                box.rotation.y = Math.PI / index;    
+            }
+            // cube.setPosition( new BABYLON.Vector3(2,2,2));
+            // var plane = BABYLON.MeshBuilder.CreatePlane("plane", { size :4}, scene);
+            // Add and manipulate meshes in the scene
+            // var plane = BABYLON.MeshBuilder.CreatePlane(
+            //     "plane", {height:2, width: 1, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
 
-                // Animation keys
-                var keys = [];
-                //At the animation key 0, the value of scaling is "1"
-                keys.push({
-                    frame: 0,
-                    value: 1
-                });
-
-                //At the animation key 20, the value of scaling is "0.2"
-                keys.push({
-                    frame: 20,
-                    value: 0.2
-                });
-
-                //At the animation key 100, the value of scaling is "1"
-                keys.push({
-                    frame: 100,
-                    value: 1
-                });
-            var animationsphere = new BABYLON.Animation("tutoAnimation", "scaling.x", 60, BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-                                    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+            // //Rotate box around the y axis
+            // plane.rotation.y = Math.PI / 3;
             
-            //Adding keys to the animation object
-            animationsphere.setKeys(keys);
-            //Then add the animation object to box1
-            sphere.animations.push(animationsphere);
-            //Finally, launch animations on box1, from key 0 to key 100 with loop activated
-            scene.beginAnimation(sphere, 0, 100, true);
+            // //Rotate box around the y axis
+            // plane.rotation.y = Math.PI / 2;
 
-            setInterval(function () {
-                //The color is defined at run time with random()
-                sphere.material.diffuseColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
-            }, 1000);
-
-            var materialSphere = new BABYLON.StandardMaterial("texture1", scene);
-            materialSphere.diffuseColor = new BABYLON.Color3(0, 1, 0);//Green
-            sphere.material = materialSphere;
-            console.log(sphere.BACKSIDE);
-            var ground = BABYLON.MeshBuilder.CreateGround("ground", {width:50, height:50}, scene);
-
+            // Add lights to the scene
+            var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+            var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+            showAxis(2);
+            
             return scene;
+
     };
 
     /******* End of the create scene function ******/    
@@ -67,5 +124,5 @@ engine.runRenderLoop(function () { // Register a render loop to repeatedly rende
 });
 
 window.addEventListener("resize", function () { // Watch for browser/canvas resize events
-        engine.resize();
+    engine.resize();
 });
