4bd30ec7debf75704b911af4553f435e17ed5263
sparanzza <aurelio@sparanzza.com>
2019-07-09 09:34:28 +0200

    convert project to TS and webpack from babylon doc

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    },
+    mode: "development"
+};
