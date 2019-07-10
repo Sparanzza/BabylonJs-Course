479c1b69477aba273daad7ad8f82623d1b88aee6
sparanzza <aurelio@sparanzza.com>
2019-07-10 10:24:28 +0200

    example with lights

--- src/game.ts
+++ src/game.ts
@@ -6,7 +6,11 @@ export class Game {
     private _engine: BABYLON.Engine;
     private _scene: BABYLON.Scene;
     private _camera: BABYLON.ArcRotateCamera;
+    // private _camera: BABYLON.UniversalCamera;
+    // private _camera: BABYLON.FlyCamera;
     private _light: BABYLON.Light;
+    private _directionalLight: BABYLON.DirectionalLight;
+    private _spotLight: BABYLON.SpotLight;
 
     constructor(canvasElement: string) {
         this._canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
@@ -21,12 +25,36 @@ export class Game {
         this._camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 4, new BABYLON.Vector3(0, 1100, 0), this._scene);
         // Target the camera to scene origin.
         this._camera.setTarget(BABYLON.Vector3.Zero());
+
+        // this._camera = new BABYLON.UniversalCamera("UniversalCamera", new BABYLON.Vector3(0, 0, -10), this._scene);
         // Attach the camera to the canvas.
-        this._camera.attachControl(this._canvas, false);
+
+        // Parameters: name, position, scene
+        // this._camera = new BABYLON.FlyCamera("FlyCamera", new BABYLON.Vector3(0, 5, -10), this._scene);
+
+        // Airplane like rotation, with faster roll correction and banked-turns.
+        // Default is 100. A higher number means slower correction.
+        // this._camera.rollCorrect = 10;
+        // Default is false.
+        // this._camera.bankedTurn = true;
+        // Defaults to 90° in radians in how far banking will roll the camera.
+        // this._camera.bankedTurnLimit = Math.PI / 2;
+        // How much of the Yawing (turning) will affect the Rolling (banked-turn.)
+        // Less than 1 will reduce the Rolling, and more than 1 will increase it.
+        // this._camera.bankedTurnMultiplier = 1;
+        this._camera.attachControl(this._canvas, false, false);
 
         // Create a basic light, aiming 0,1,0 - meaning, to the sky.
         this._light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this._scene);
-        this._light.intensity = 0.7;
+        this._light.intensity = 0.35;
+        this._directionalLight = new BABYLON.DirectionalLight("DirectionalLight", new BABYLON.Vector3(0, -1, 0), this._scene);
+        this._directionalLight.intensity = 0.35;
+
+        this._spotLight = new BABYLON.SpotLight("spotLight", new BABYLON.Vector3(0, 500, -10), new BABYLON.Vector3(0, -1, 0), Math.PI / 3, 100, this._scene);
+        this._spotLight.intensity = 1;
+        this._spotLight.diffuse = new BABYLON.Color3(1, 1, 0);
+        this._spotLight.specular = new BABYLON.Color3(1, 1, 0);
+        this._spotLight.range = 10;
 
         // Golden Material
         let goldenMaterial = new BABYLON.StandardMaterial("golden", this._scene);
@@ -49,7 +77,7 @@ export class Game {
 
         ground.material = velvetMaterial;
 
-        let _globalAxis = new GlobalAxis(10, this._scene);
+        let _globalAxis = new GlobalAxis(100, this._scene);
     }
 
     doRender(): void {
